c4f8065d31545899c094fd8e15393466401beb34
unknown <jharanaadhikari.xdezo@gmail.com>
2023-06-16 19:55:22 +0545

    first commit

--- library/home/urls.py
+++ library/home/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('book/', views.book, name="book"),
     path('contact/', views.contact, name="contact"),
-    path('author/', views.author, name="author")
+    path('author/', views.author, name="author"),
+    path('createauthor/', views.createauthor, name="createauthor"),
+    path('readauthor/<int:author_id>/', views.readauthor, name="readauthor"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, documnet_root=settings.MEDIA_ROOT)
--- library/home/views.py
+++ library/home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Books, Author
+from . forms import AuthorForm
 # Create your views here.
 def index(request):
     return render(request, "index.html")
@@ -21,3 +22,20 @@ def book(request):
     book = Books.objects.all()
     context= {'book':book}
     return render(request, 'book.html', context)
+
+def createauthor(request):
+    if request.method =="GET":
+        author_form = AuthorForm
+        return render(request, 'create_author.html', context={'form': author_form})
+    else:
+        author_form = AuthorForm(request.POST)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect('author')
+    return render(request, 'create_author.html', context={'form': author_form})
+
+
+def readauthor(request, author_id):
+    author = Author.objects.get(id=author_id)
+    context ={'author':author}
+    return render(request, 'readauthor.html', context)
